1a196f7f4012e520ba7cedb04313f7d70f91aa15
Sashko Stubailo <sashko@mit.edu>
2014-03-29 22:47:52 -0400

    Making a new app now goes to the right folder

--- meteor.sh
+++ meteor.sh
@@ -4,15 +4,6 @@ then
   curl https://install.meteor.com/ | sh
 fi
 
-# make the first app
-[ -f /home/vagrant/app ] && rm -r /home/vagrant/app
-meteor create /home/vagrant/app
-
-[ -f /vagrant/app ] && rm -r /vagrant/app
-meteor create /vagrant/app
-
-sudo mount --bind /home/vagrant/app/.meteor /vagrant/app/.meteor
-
-cd /vagrant/app
-ls
-meteor update
+mkdir /vagrant/apps
+mkdir /home/vagrant/.instacode
+mkdir /home/vagrant/.instacode/apps
